339b60efbe652e2084fa3a0435af13a285db5068
Gerardo <gcuamea09@gmail.com>
2025-03-14 14:11:31 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img className='menu' src="/assets/menu.png" alt="Menu Restaurante" />
+      <a href='tel:+523324835530'>
+       <img className='phone' src="/assets/phone.png" alt="Llamar" />
+      </a>
+    </>
+  )
+}
+
+export default App
