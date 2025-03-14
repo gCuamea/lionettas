1e2fe9c17a5384ef9ba6778ebdd953c2a84a3e1c
Gerardo <gcuamea09@gmail.com>
2025-03-14 14:30:58 -0600

    src update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import menu from '/assets/menu.png'
+import phone from '/assets/phone.png'
 
 function App() {
   return (
     <>
-      <img className='menu' src="/assets/menu.png" alt="Menu Restaurante" />
+      <img className='menu' src={menu} alt="Menu Restaurante" />
       <a href='tel:+523324835530'>
-       <img className='phone' src="/assets/phone.png" alt="Llamar" />
+       <img className='phone' src={phone} alt="Llamar" />
       </a>
     </>
   )
